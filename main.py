37fad2b23b19de3519dd298397d55934b97a7c2b
Russell Molimock <495@holbertonschool.com>
2022-04-20 23:36:34 -0500

    moved initial model to separate module

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from config import ConfigFile
 from db.credentials import DatabaseCredentials
 
 
+
 CONFIG_FILENAME = 'config.ini'
 
 
@@ -18,18 +19,3 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.name
-
-
-db.create_all()
-
-admin = User(id=2, name='admin@example.com')
-db.session.add(admin)
-db.session.commit()
-
-a = User.query.all()
