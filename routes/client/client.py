9a084e79fc47660d846680e516ba2781e43d4429
Russell Molimock <russellmolimock@gmail.com>
2024-04-16 11:57:33 -0400

    PUT to update client name by id

--- routes/client/client.py
+++ routes/client/client.py
@@ -1,47 +1,12 @@
+'''
+POST create
+GET all clients
+GET, DELETE, PUT client by id
+'''
+
+
 from routes.client import clients_v1
 
-@clients_v1.route("/", methods=["GET"], strict_slashes=False)
-def all_clients():
-    '''
-    Get all clients
-    '''
-    from models import Client
-
-    clients = Client.load_all_dict()
-    if not clients:
-        return "No clients found", 404
-
-    return clients, 200
-
-@clients_v1.route("/<id>", methods=["GET", "DELETE"], strict_slashes=False)
-def get_by_id(id):
-    '''
-    Get, update, or delete a client by id
-    '''
-    from flask import request
-    from models import Client
-
-    client = Client.load_by_id(id)
-    if not client:
-        return "Not found. Invalid client ID.", 404
-    
-    if request.method == "DELETE":
-        token = request.headers.get("Authorization")
-        if not token:
-            return "Unauthorized", 401
-        
-        if not isinstance(token, str) and len(token) > 7:
-            return "Unauthorized", 401
-        
-        token = token[7:] # remove the 'Bearer ' prefix
-
-        if token != client.token:
-            return "Unauthorized" + token + ' --- ' + client.token, 401
-        
-        client.delete()
-        return "Client deleted", 204
-    
-    return client.to_dict(), 200
 
 @clients_v1.route("/", methods=["POST"], strict_slashes=False)
 def create_client():
@@ -67,3 +32,60 @@ def create_client():
     client.save()
 
     return client_dict, 201
+
+@clients_v1.route("/", methods=["GET"], strict_slashes=False)
+def all_clients():
+    '''
+    Get all clients
+    '''
+    from models import Client
+
+    clients = Client.load_all_dict()
+    if not clients:
+        return "No clients found", 404
+
+    return clients, 200
+
+@clients_v1.route("/<id>", methods=["GET", "DELETE", ], strict_slashes=False)
+def get_by_id(id):
+    '''
+    Get, update, or delete a client by id
+    '''
+    from flask import request
+    from models import Client
+
+    client = Client.load_by_id(id)
+    if not client:
+        return "Not found. Invalid client ID.", 404
+    
+    if request.method == "GET":
+        return client.to_dict(), 200
+    
+    token = request.headers.get("Authorization")
+    if not token:
+        return "Unauthorized", 401
+    
+    if not isinstance(token, str) and len(token) > 7:
+        return "Unauthorized", 401
+    
+    token = token[7:] # remove the 'Bearer ' prefix
+
+    if token != client.token:
+        return "Unauthorized" + token + ' --- ' + client.token, 401
+    
+    if request.method == "DELETE":
+        client.delete()
+        return "Client deleted", 204
+    elif request.method == "PUT":
+        name = request.form.get("name")
+        if not name:
+            return "Client name is required", 400
+        
+        existing_client = Client.load_by_attr('name', name)
+        if existing_client:
+            return "Client name already in use.", 400
+
+        client.name = name
+        client.save()
+        return client.to_dict(), 200
+    
