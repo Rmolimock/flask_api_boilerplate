f58ae8bb57a16a99186fd7bcb9029ca4b1b6e367
Russell Molimock <russellmolimock@gmail.com>
2024-04-28 23:08:13 -0400

    separating requests into a shared pytest fixture. Next will parameterize valid/invalid requests, authorized/unauthorized ones, and even method types themsleves for scalability, then move parameters into a shared BaseTest class so I can share that functionality with my user tests

--- tests/test_client_api.py
+++ tests/test_client_api.py
@@ -1,6 +1,6 @@
-"""
+'''
 Tests for the client API endpoints, GET all and GET, PUT, DELETE by id
-"""
+'''
 
 import pytest
 from main import app
@@ -11,7 +11,7 @@ from uuid import uuid4
 def test_app():
     # configure the app for testing
     # SETUP
-    app.config["TESTING"] = True
+    app.config['TESTING'] = True
 
     # CLEAN UP
     yield app
@@ -27,19 +27,37 @@ def api(test_app):
 @pytest.fixture()
 def mock_client(mocker):
     # Initialize a shared mock client for TestClients tests
-    mock_client = mocker.patch("routes.client.client.Client")
+    mock_client = mocker.patch('routes.client.client.Client')
     return mock_client
 
 
 @pytest.fixture()
 def mock_client_before_request(mocker):
     # Initialize a shared mock client for TestClients tests
-    mock_client = mocker.patch("before_requests.Client")
+    mock_client = mocker.patch('before_requests.Client')
     return mock_client
+
+@pytest.fixture()
+def make_request(api):
+    '''
+    Return a function that can make requests to the API. Optionally includes an authorization header.
+    '''
+    def request(method, route, token=None, data=None):
+        if method not in ['get', 'post', 'put', 'delete']:
+            raise ValueError('Invalid HTTP method specified.')
+
+        headers = {}
+        if token:
+            headers['Authorization'] = f'Bearer {token}'
+
+        method_func = getattr(api, method)
+        return method_func(route, headers=headers, json=data)
+
+    return request
 
 
 class TestClients:
-    """
+    '''
     Test the client API endpoints
     1. GET all clients
     2. GET client by id
@@ -47,81 +65,81 @@ class TestClients:
     4. DELETE client by id
     - [ ] test all both with and without authorization (required by PUT and DELETE)
     - [ ] test GET, PUT, and DELETE by id with both valid and invalid client id
-    """
+    '''
 
     def test_unauthorized_get_all(self, api):
-        """
+        '''
         Test the GET all clients endpoint without authorization
-        """
+        '''
         # MOCK VALUES (none)
         # ACTIONS
-        response = api.get("/v1/clients/")
+        response = api.get('/v1/clients/')
 
         # ASSERTIONS
         assert response.status_code == 200
 
     def test_authorized_get_all(self, api, mock_client_before_request):
-        """
+        '''
         Test the GET all clients endpoint with authorization
-        """
+        '''
         # MOCK VALUES
         mock_token = str(uuid4())
-        headers = {"Authorization": f"Bearer {mock_token}"}
+        headers = {'Authorization': f'Bearer {mock_token}'}
 
         # ACTIONS
-        response = api.get("/", headers=headers)
+        response = api.get('/', headers=headers)
 
         # ASSERTIONS
         assert response.status_code == 200
         mock_client_before_request.load_by_attr.assert_called_once_with(
-            "token", mock_token
+            'token', mock_token
         )
 
     def test_unauthorized_get_by_id(self, api, mock_client):
-        """
+        '''
         Test the GET client by id endpoint without authorization
-        """
+        '''
         # MOCK VALUES
         mock_id = str(uuid4())
         mock_client.load_by_id.return_value = mock_client
 
         # ACTIONS
-        response = api.get(f"/v1/clients/{mock_id}")
+        response = api.get(f'/v1/clients/{mock_id}')
 
         # ASSERTIONS
         assert response.status_code == 200
         mock_client.load_by_id.assert_called_once_with(mock_id)
 
     def test_authorized_get_by_id(self, api, mock_client, mock_client_before_request):
-        """
+        '''
         Test the GET client by id endpoint with authorization
-        """
+        '''
         # MOCK VALUES
         mock_id = str(uuid4())
         # Bool would cause error, and must be mocked to avoid genuinely loading a client
         mock_client.load_by_id.return_value = mock_client
         mock_token = str(uuid4())
-        headers = {"Authorization": f"Bearer {mock_token}"}
+        headers = {'Authorization': f'Bearer {mock_token}'}
 
         # ACTIONS
-        response = api.get(f"/v1/clients/{mock_id}", headers=headers)
+        response = api.get(f'/v1/clients/{mock_id}', headers=headers)
 
         # ASSERTIONS
         assert response.status_code == 200
         mock_client.load_by_id.assert_called_once_with(mock_id)
         mock_client_before_request.load_by_attr.assert_called_once_with(
-            "token", mock_token
+            'token', mock_token
         )
 
     def test_unauthorized_put_by_id(self, api, mock_client, mock_client_before_request):
-        """
+        '''
         Test the PUT client by id endpoint without authorization
-        """
+        '''
         # MOCK VALUES
         mock_id = str(uuid4())
 
         # ACTIONS
-        response = api.put(f"/v1/clients/{mock_id}")
+        response = api.put(f'/v1/clients/{mock_id}')
 
         # ASSERTIONS
         assert response.status_code == 401
@@ -139,14 +157,14 @@ class TestClients:
         json = {'name': 'test_name'}
 
         mock_client.load_by_id.return_value = mock_client
-        # before_request checks for valid client by authenticator token
+        # before_request checks for valid client by authorization token
         # must be client object because it then checks client.token
         mock_client_before_request.load_by_attr.return_value = mock_client
         # route checks if the proposed client name is already in use
         mock_client.load_by_attr.return_value = False
 
         # ACTIONS
-        response = api.put(f"/v1/clients/{mock_id}", headers=headers, data=json)
+        response = api.put(f'/v1/clients/{mock_id}', headers=headers, data=json)
         print(response.data)
 
         # ASSERTIONS
