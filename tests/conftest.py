dc7cf3f63ac8d15df2063f091d1e25ecff5797c3
Russell Molimock <russellmolimock@gmail.com>
2024-07-14 05:15:35 -0400

    authorization is now injected into make_request helper so it can be accessed for test conditionals too. Helper fixture for methods without post. get_mock_obj always sets mock class return value now, only sets obj.id if obj_id present.

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,8 @@ import pytest
 from uuid import uuid4
 
 
+# BASICS, App and API =========================================================
+
 @pytest.fixture()
 def test_app():
     from main import app
@@ -25,15 +27,19 @@ def api(test_app):
     # to avoid confusion with client objects
     return test_app.test_client()
 
+
+# SIMPLE mock values for fixtures =============================================
+
 pytest.fixture()
 def mock_id():
     return str(uuid4())
 
-
-
 @pytest.fixture()
 def authentication_token():
     return str(uuid4())
+
+
+# Helper functions for fixtures ===============================================
 
 @pytest.fixture()
 def get_mock_class(mocker):
@@ -44,6 +50,71 @@ def get_mock_class(mocker):
         return mocker.patch(mock_class_path)
     return mock_class
 
+@pytest.fixture()
+def get_mock_object(mocker):
+    """
+        A function to mock a client object from a given class, mock the class'
+        load_by_id method to return it, and set the object's attributes to the
+        values with kwargs. Set id if obj_id present.
+        Return: mock object function
+        """
+    def mock_object(mock_class, obj_id=None, **kwargs):
+        """
+        Mock a client object from a given class, mock the class' load_by_id
+        method to return it, and set the object's attributes to the values
+        with kwargs. Set id if obj_id present.
+        Return: mock object
+        """
+        mock_obj = mocker.MagicMock()
+        mock_class.load_by_id.return_value = mock_obj
+        if obj_id:
+            mock_obj.id = obj_id
+        for key, value in kwargs.items():
+            setattr(mock_obj, key, value)
+            mock_class.load_by_attr.return_value = mock_obj
+        return mock_obj
+    return mock_object
+
+
+# Parameterized fixtures ======================================================
+
+@pytest.fixture(params=["get", "post", "put", "delete"])
+def method(request):
+    """
+    Parameterize the methods only, not routes. So GET /obj and GET /obj/{id}
+    should be tested separately, not parameterized.
+    """
+    return request.param
+
+@pytest.fixture(params=["get", "put", "delete"])
+def method_no_post(request):
+    """
+    Parameterize the methods only, not routes. So GET /obj and GET /obj/{id}
+    should be tested separately, not parameterized.
+    """
+    return request.param
+
+@pytest.fixture(params=[True, False])
+def has_valid_id(request, mocker, get_mock_class, get_mock_object):
+
+    def is_valid_id(mock_class_path):
+        """
+        Simulate loading an object from the db with a valid ID. Mock it's
+        class to return a mock object when using load_by_id, which has an
+        id equal to the string returned by this function, which should be
+        used within test functions for requests by id in the route arg of
+        make_authenticated_request().
+        """
+        if request.param:
+            obj_id = str(uuid4())
+            mock_class = get_mock_class(mock_class_path)
+            mock_obj = get_mock_object(mock_class, obj_id)
+            return obj_id
+        else:
+            return None
+            
+    return is_valid_id
+
 '''
 parameterize this to have authorization token or not
 then use get_mock_class with the route for the client inside before_request
@@ -52,13 +123,22 @@ or not depending on the request.param. Then anytime a request is sent within a t
 it will do two requests, one with a token and mocked client with the correct client.token
 and one with no token sent and a mocked client class that does not return a client object at all.
 '''
+
 @pytest.fixture(params=[True, False])
-def make_authenticated_request(api, get_mock_class, authentication_token):
+def authorized(request):
+    """
+    Parameterize authorization helper for make_requests. This must be separate in order
+    to check the conditional "if authorized" inside tests, for assertions dependent on that.
+    """
+    return request.param
+
+@pytest.fixture()
+def make_request(api, get_mock_class, authentication_token, authorized):
     """
     Return a function that can make requests to the API. Optionally includes an authorization header.
     """
 
-    def request(method, route, data=None):
+    def request_func(method, route, data=None):
         """
         Simulate making a request to the API with the Authorization header present only when token is present.
         """
@@ -75,7 +155,7 @@ def make_authenticated_request(api, get_mock_class, authentication_token):
             raise TypeError("Data must be a dictionary")
 
         headers = {}
-        if request.param:
+        if authorized:
             # will I need access to the token in the test function?
             # I'll need it here to mock a response, and I'll want it in the test
             # function so I can assert that the client was loaded with the correct token
@@ -87,50 +167,9 @@ def make_authenticated_request(api, get_mock_class, authentication_token):
             mock_client = get_mock_object(before_client_class, **attrs)
 
         method_func = getattr(api, method)
+        print(headers, "********** * ************* ********")
         return method_func(route, headers=headers, json=data)
 
-    return request
+    return request_func
 
-
-@pytest.fixture()
-def get_mock_object(mocker):
-    def mock_object(mock_class, obj_id=None, **kwargs):
-        """
-        Mock a client object and return it.
-        """
-        mock_obj = mocker.MagicMock()
-        if obj_id:
-            mock_class.load_by_id.return_value = mock_obj
-            mock_obj.id = obj_id
-        for key, value in kwargs.items():
-            setattr(mock_obj, key, value)
-            mock_class.load_by_attr.return_value = mock_obj
-        return mock_obj
-    return mock_object
-
-
-@pytest.fixture(params=[True, False])
-def has_valid_id(request, mocker, get_mock_class, get_mock_object):
-
-    def is_valid_id(mock_class_path):
-        """
-        Simulate loading an object with a valid ID. Mock it's class to
-        return a mock object when using load_by_id, which has an id equal
-        to the string returned by this function, which should be used within
-        test functions for requests by id in the route arg of make_request().
-        """
-        if request.param:
-            obj_id = str(uuid4())
-            mock_class = get_mock_class(mock_class_path)
-            mock_obj = get_mock_object(mock_class, obj_id)
-            return obj_id
-        else:
-            return None
-            
-    return is_valid_id
-
-
-@pytest.fixture(params=["get", "post", "put", "delete"])
-def method(request):
-    return request.param
 
--- tests/test_clients.py
+++ tests/test_clients.py
@@ -1,15 +1,15 @@
 from uuid import uuid4
 
 
-def test_get_client(has_valid_id, make_request, get_mock_class, method, authentication_token, get_mock_object):
+def test_client_methods(has_valid_id, make_request, get_mock_class, method_no_post, authorized, authentication_token, get_mock_object):
     # MOCK VALUES
     client_class_route = 'routes.client.client.Client'
-    valid_client_id = has_valid_id(client_class_route)
+    valid_client_id = has_valid_id(client_class_route) # a parameter
     authenticator_class = get_mock_class("before_requests.Client")
     attrs = {
         'token': authentication_token,
     }
-    authenticated_client = get_mock_object(client_class_route, **attrs)
+    authenticated_client = get_mock_object(authenticator_class, **attrs)
 
 
 
@@ -22,13 +22,12 @@ def test_get_client(has_valid_id, make_request, get_mock_class, method, authenti
     client_id = valid_client_id if valid_client_id else invalid_client_id
 
     # ACTION
-    response = make_request(method, f"/v1/clients/{client_id}")
+    response = make_request(method_no_post, f"/v1/clients/{client_id}")
 
     # ASSERTIONS
-    if method == "post":
-        assert response.status_code == 405
-        return
     assert response.status_code == 200 if valid_client_id else 404
     mock_client_class.load_by_id.assert_called_once_with(client_id)
-    authenticator_class.load_by_attr.assert_called_once_with("token", authentication_token)
-    assert authenticated_client.token == authentication_token
+    # if authorization header was present:
+    if authorized:
+        authenticator_class.load_by_attr.assert_called_once_with("token", authentication_token)
+        assert authenticated_client.token == authentication_token
