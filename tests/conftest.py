b2f56cb15108ae0162c28139a13932ca07389730
Russell Molimock <russellmolimock@gmail.com>
2024-07-25 05:25:58 -0400

    now mocking instance and class correctly and asserting correct id and to_dict was called for route response

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,6 +16,7 @@ from uuid import uuid4
 @pytest.fixture()
 def test_app():
     from main import app
+
     # configure the app for testing
     # SETUP
     app.config["TESTING"] = True
@@ -37,11 +38,9 @@ def api(test_app):
 
 
 pytest.fixture()
-def mock_id():
-    return str(uuid4())
 
-@pytest.fixture()
-def authorization_token():
+
+def mock_id():
     return str(uuid4())
 
 
@@ -52,21 +51,25 @@ def authorization_token():
 
 def load_mock_obj_by_attrs(mock_class, **kwargs):
     from unittest.mock import MagicMock
+
     mock_obj = MagicMock()
+    mock_obj.to_dict = MagicMock(return_value=kwargs)
 
     for key, value in kwargs.items():
         setattr(mock_obj, key, value)
         mock_class.load_by_attr.return_value = mock_obj
-        mock_class.load_by_attr = mock_obj
     return mock_obj
+
 
 def load_mock_obj_by_id(mock_class, id):
     from unittest.mock import MagicMock
+
     mock_obj = MagicMock()
     mock_obj.id = id
+    mock_obj.to_dict = MagicMock(return_value={"id": id})
     mock_class.load_by_id.return_value = mock_obj
-    mock_class.load_by_id = mock_obj
     return mock_obj
+
 
 # =============================================================================
 # Parameterized fixtures ======================================================
@@ -81,6 +84,7 @@ def method(request):
     """
     return request.param
 
+
 @pytest.fixture(params=["get", "put", "delete"])
 def method_no_post(request):
     """
@@ -89,9 +93,11 @@ def method_no_post(request):
     """
     return request.param
 
+
 @pytest.fixture(params=[True, False])
 def is_valid_id(request):
     return str(uuid4()) if request.param else None
+
 
 @pytest.fixture()
 def make_request(api):
@@ -105,10 +111,10 @@ def make_request(api):
         """
         if not isinstance(method, str):
             raise TypeError("Method must be a string")
-        
+
         if method not in ["get", "post", "put", "delete"]:
             raise ValueError("Invalid HTTP method specified.")
-        
+
         if not isinstance(route, str):
             raise TypeError("Route must be a string")
 
@@ -116,7 +122,7 @@ def make_request(api):
             raise TypeError("Data must be a dictionary")
 
         headers = {}
-        '''if authorized:
+        """if authorized:
             # will I need access to the token in the test function?
             # I'll need it here to mock a response, and I'll want it in the test
             # function so I can assert that the client was loaded with the correct token
@@ -126,12 +132,13 @@ def make_request(api):
                 'token': authorization_token,
             }
             mock_client = get_mock_object(before_client_class, **attrs)
-        '''
+        """
 
         method_func = getattr(api, method)
         return method_func(route, headers=headers, json=data)
 
     return request_func
+
 
 @pytest.fixture()
 def mock_obj_if_valid_id(is_valid_id):
@@ -140,30 +147,5 @@ def mock_obj_if_valid_id(is_valid_id):
             return load_mock_obj_by_id(mock_class, is_valid_id)
         else:
             return None
+
     return func
-
-
-
-
-
-
-
-'''
-    def is_valid_id(mock_class_path):
-        """
-        Simulate loading an object from the db with a valid ID. Mock it's
-        class to return a mock object when using load_by_id, which has an
-        id equal to the string returned by this function, which should be
-        used within test functions for requests by id in the route arg of
-        make_authenticated_request().
-        """
-        if request.param:
-            obj_id = str(uuid4())
-            mock_class = get_mock_class(mock_class_path)
-            mock_obj = get_mock_object(mock_class, obj_id)
-            return obj_id
-        else:
-            return None
-            
-    return is_valid_id
-'''
--- tests/test_clients.py
+++ tests/test_clients.py
@@ -2,42 +2,44 @@ from uuid import uuid4
 import pytest
 
 
-def test_client_method(mocker, is_valid_id, method_no_post, make_request, mock_obj_if_valid_id):
+def test_client_method(
+    mocker, is_valid_id, method_no_post, mock_obj_if_valid_id, make_request
+):
     # MOCK VALUES
     client_class_in_route = mocker.patch("routes.client.client.Client")
     method = method_no_post
 
     # SETUP
-    valid_client_in_route = mock_obj_if_valid_id(client_class_in_route)
-    client_id = valid_client_in_route.id if is_valid_id else str(uuid4())
+    client_instance_in_route = mock_obj_if_valid_id(client_class_in_route)
+    client_id = client_instance_in_route.id if is_valid_id else str(uuid4())
 
     # ACTION
     response = make_request(method, f"/v1/clients/{client_id}")
 
     # ASSERTIONS
-    if method == 'get':
+    if method == "get":
+
         assert response.status_code == 200 if is_valid_id else 404
+        client_class_in_route.load_by_id.assert_called_once_with(client_id)
+        client_instance_in_route.to_dict.assert_called_once() if is_valid_id else None
     else:
+
         assert response.status_code == 401
     client_class_in_route.load_by_id.assert_called_once_with(client_id)
 
 
-
-
-
-
-'''
+"""
 authorizing_client = get_mock_class("before_requests.Client")
 attrs = {
     'token': authorization_token,
 }
 authenticated_client = get_mock_object(authorizing_client, **attrs)
-'''
-'''
+"""
+"""
 if authorized:
     print("Checking authorizing_client.load_by_attr calls")
     print(f"Authorizing client mock after request: {authorizing_client}")
     print(f"Mock calls: {authorizing_client.load_by_attr.mock_calls}")
     authorizing_client.load_by_attr.assert_called_once_with("token", authorization_token)
     assert authenticated_client.token == authorization_token
-'''
+"""
